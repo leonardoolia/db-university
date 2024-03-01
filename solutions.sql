a995344dd910ef18d0d05c21e4fb0b673efa7f3d
leonardoolia <leonardooliaa@gmail.com>
2024-03-01 14:59:20 +0100

    ex. 2.4

--- solutions.sql
+++ solutions.sql
@@ -35,4 +35,4 @@ SELECT COUNT(id) AS 'numero_insegnanti', `office_address` FROM `teachers` GROUP 
 SELECT ROUND(AVG(`vote`), 2) AS 'media_voti', `exam_id` FROM `exam_student` GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-
+SELECT COUNT(`name`) AS 'num_corsi_di_laurea', `department_id` FROM `degrees` GROUP BY `department_id`;
