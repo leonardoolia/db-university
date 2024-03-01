b039c0a6ba26ffcd242dfceeaed6e84e39c24335
leonardoolia <leonardooliaa@gmail.com>
2024-03-01 14:15:03 +0100

    ex. 1.7

--- solutions.sql
+++ solutions.sql
@@ -18,7 +18,7 @@ SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` > '14:00:00';
 SELECT * FROM `degrees` WHERE `name` LIKE '%Magistrale%';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(id) AS 'dipartimenti_totali' FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
